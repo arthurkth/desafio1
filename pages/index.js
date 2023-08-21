c2a33c8940a7d985024504dde40340411872ba8b
Arthur Kunrath <57488934+arthurkth@users.noreply.github.com>
2023-08-21 23:16:16 +0000

    Brincadeira finalizada

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div style={{width: '100%', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <img src='/assets/cachorro.jpg' style={{maxWidth: '450px'}}/>
+            <h1 style={{color: '#f17ea1', fontSize: '3rem'}}>Te amo</h1>
+        </div>
+    )
+}
+
+export default Home;
